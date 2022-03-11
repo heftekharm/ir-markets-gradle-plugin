4b31d7eadaaa44df894f6a35055b921d3cabe003
Hosein <h.eftekhar.m@gmail.com>
2022-03-11 23:32:57 +0330

    add publish logic

--- src/main/java/PublishBazaarTask.kt
+++ src/main/java/PublishBazaarTask.kt
@@ -1,4 +1,7 @@
 import com.android.build.api.variant.BuiltArtifactsLoader
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.mime.MultipartEntityBuilder
+import org.apache.http.impl.client.HttpClients
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
@@ -9,6 +12,7 @@ import org.gradle.api.tasks.TaskAction
 import java.io.File
 import java.lang.RuntimeException
 
+private const val API_KEY_HEADER="CAFEBAZAAR-PISHKHAN-API-SECRET"
 abstract class PublishBazaarTask() : DefaultTask() {
 
     @get:Input
@@ -22,11 +26,33 @@ abstract class PublishBazaarTask() : DefaultTask() {
 
     @TaskAction
     fun publish() {
-        println("bazaarApiKey is : ${bazaarKey.get()}")
+        val bazaarKeyValue=bazaarKey.get()
+        println("bazaarApiKey is : $bazaarKeyValue")
         val builtArtifacts = builtArtifactsLoader.get().load(apkFolder.get()) ?: throw RuntimeException()
-        builtArtifacts.elements.single().let {
-            val apk = File(it.outputFile).toPath()
-            println("publishing ${apk.fileName} on Bazaar Market")
+        builtArtifacts.elements.single().let { buildArtifact ->
+            val apk = File(buildArtifact.outputFile)
+            println("publishing ${apk.name} on Bazaar Market")
+            val httpClient = HttpClients.createDefault()
+            HttpPost("https://api.pishkhan.cafebazaar.ir/v1/apps/releases").apply {
+                addHeader(API_KEY_HEADER, bazaarKeyValue)
+            }.let { portReq ->
+                httpClient.execute(portReq)
+            }.takeIf {
+                it.statusLine.statusCode == 201
+            }?.let {
+                HttpPost("https://api.pishkhan.cafebazaar.ir/v1/apps/releases/upload")
+            }?.apply {
+                addHeader(API_KEY_HEADER, bazaarKeyValue)
+                val contentEntity = MultipartEntityBuilder.create()
+                    .addBinaryBody("apk", apk)
+                    .addTextBody("architecture", "all")
+                    .build()
+                entity = contentEntity
+            }.let {
+                httpClient.execute(it)
+            }.let {
+                println(it.statusLine)
+            }
         }
         println("SUCCESS")
     }
