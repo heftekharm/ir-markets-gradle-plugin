2da63a006ad7b8ec27f2ec7b79b1119f8c2b5631
Hosein <h.eftekhar.m@gmail.com>
2022-04-06 02:32:16 +0430

    add commit service

--- src/main/java/CafeBazaarApiService.kt
+++ src/main/java/CafeBazaarApiService.kt
@@ -1,18 +1,24 @@
+import org.apache.http.HttpRequestInterceptor
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.ContentType
+import org.apache.http.entity.StringEntity
 import org.apache.http.entity.mime.MultipartEntityBuilder
 import org.apache.http.entity.mime.content.FileBody
-import org.apache.http.impl.client.HttpClients
+import org.apache.http.impl.client.HttpClientBuilder
+import org.gradle.internal.impldep.com.google.gson.JsonObject
 import java.io.File
 
 private const val API_KEY_HEADER = "CAFEBAZAAR-PISHKHAN-API-SECRET"
 
 class CafeBazaarApiService(private val cafeBazaarApiKey: String) {
-    private val httpClient = HttpClients.createDefault()
+
+    private val httpClient = HttpClientBuilder.create()
+        .addInterceptorFirst(HttpRequestInterceptor { request, context ->
+            request.addHeader(API_KEY_HEADER, cafeBazaarApiKey)
+        }).build()
 
     fun requestUploadingRelease():Boolean {
         HttpPost("https://api.pishkhan.cafebazaar.ir/v1/apps/releases/").apply {
-            addHeader(API_KEY_HEADER, cafeBazaarApiKey)
         }.let { portReq ->
             httpClient.execute(portReq)
         }.let {
@@ -26,7 +32,6 @@ class CafeBazaarApiService(private val cafeBazaarApiKey: String) {
         println("publishing ${apk.name} on Bazaar Market")
         HttpPost("https://api.pishkhan.cafebazaar.ir/v1/apps/releases/upload/")
             .apply {
-                addHeader(API_KEY_HEADER, cafeBazaarApiKey)
                 val contentEntity = MultipartEntityBuilder.create()
                     .addTextBody("architecture", "all")
                     .addPart("apk",FileBody(apk, ContentType.create("application/vnd.android.package-archive")))
@@ -38,5 +43,26 @@ class CafeBazaarApiService(private val cafeBazaarApiKey: String) {
                 println(it.statusLine)
                 return it.statusLine.statusCode in 200 until 202
             }
+    }
+
+    fun commit(changelogEn:String="",changelogFa:String="",developerNote:String="",stagedRolloutPercentage:Int=100,autoPublish:Boolean=false):Boolean{
+        println("Committing the release")
+        val stringedContentJson="""
+            {
+                "staged_rollout_percentage": $stagedRolloutPercentage,
+                "changelog_fa": "$developerNote",
+                "changelog_en": "$changelogEn",
+                "developer_note": "$changelogFa",
+                "auto_publish": $autoPublish
+            }
+        """.trimIndent()
+        HttpPost("https://api.pishkhan.cafebazaar.ir/v1/apps/releases/commit").apply {
+            entity=StringEntity(stringedContentJson,ContentType.APPLICATION_JSON)
+        }.let {
+            httpClient.execute(it)
+        }.let {
+            println(it.statusLine)
+            return it.statusLine.statusCode in 200 until 202
+        }
     }
 }
