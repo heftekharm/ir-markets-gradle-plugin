cbe21f4040984262abce14be485f2594d579618e
Hosein <h.eftekhar.m@gmail.com>
2022-04-06 02:42:38 +0430

    add commit params to extension

--- src/main/java/GradleIrMarketsPublisherExtension.kt
+++ src/main/java/GradleIrMarketsPublisherExtension.kt
@@ -1,3 +1,10 @@
+import org.gradle.api.file.RegularFile
+
 abstract class GradleIrMarketsPublisherExtension {
     var cafeBazaarApiKey: String? = null
+    var changeLogFaTextFile: RegularFile? = null
+    var changeLogEnTextFile: RegularFile? = null
+    var autoPublish: Boolean = false
+    var stagedRolloutPercentage: Int = 100
+    var developerNoteTextFile: RegularFile? = null
 }
